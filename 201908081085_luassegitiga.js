581b01798c9e7f295ea950dfb06dfbbaea5a2ef9
julius daliofa <54829663+juliusdaliofa@users.noreply.github.com>
2019-09-10 13:12:07 +0700

    Delete 201908081085_luassegitiga.js

--- 201908081085_luassegitiga.js
+++
@@ -1,6 +0,0 @@
-var keyboard=require('readline-sync');
-var a = parseInt(keyboard.question("Masukan Alas : "));
-var t = parseInt(keyboard.question("Masukan Tinggi : "));
-var luas = a*t/2;
-
-console.log("Luas Segitiga = "+luas);
