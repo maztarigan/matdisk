78cfa3bb53fa9438b3ce219bfc287aea4e9a9d72
khairul fadhly <54829667+khairulfadhly@users.noreply.github.com>
2019-09-10 13:06:21 +0700

    Delete 20190801235_luassegitiga.js

--- 20190801235_luassegitiga.js
+++
@@ -1,8 +0,0 @@
-var bacakeyboard = require ('readline-sync');
-
-var a = 3;
-var t = parseInt(bacakeyboard.question("masukan Alas ;"));
-
-var luas =1/2 * a * t
-
-console.log("luas segitiga dengan alas =" +luas);
