6b7c13893272a56b0acbdee873fa87da57905d3a
Muhamad Magfur <54829786+muhamadmagfur@users.noreply.github.com>
2019-09-17 12:48:37 +0700

    Add files via upload

---
+++ 20190801021_nilai.js
@@ -0,0 +1,16 @@
+var inputkeyboard = require('readline-sync');
+
+var nilai = parseInt(inputkeyboard.question("masukan nilai"));
+
+if ((nilai >= 81) && (nilai <= 100 )){
+    console.log("nilai A");
+}else if ((nilai >= 70 ) && (nilai <= 80 )){
+    console.log("nilai B");
+}else if ((nilai >= 60 ) && (nilai <= 69 )){
+    console.log("nilai C");
+}else if ((nilai >= 50 ) && (nilai <= 59 )){
+    console.log("nilai D");
+}else [
+    console.log("Nilai E")
+]
+
