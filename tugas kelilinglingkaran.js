141449524394d370e4355a983c8d1ce468433ae5
deliaapriana <54826744+deliaapriana@users.noreply.github.com>
2019-09-17 10:44:06 +0700

    Add files via upload
    20190803037

---
+++ tugas kelilinglingkaran.js
@@ -0,0 +1,7 @@
+var bacakeyboard =require('readline-sync');
+
+var pi = 3.14
+var d = parseInt(bacakeyboard.question("masukkan nilai diameter: "));
+var keliling = pi* d;
+
+console.log(keliling);
---
+++ tugas kubus.js
@@ -0,0 +1,6 @@
+var bacakeyboard = require('readline-sync');
+
+var s = parseInt(bacakeyboard.question("masukkan nilai sisi: "));
+var isi = s * s * s
+
+console.log(isi);
