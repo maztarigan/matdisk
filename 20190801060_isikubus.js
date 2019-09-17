28cf9ce7288f204b4d4038becc8c2590f983edb6
danangabdul <54830216+danangabdul@users.noreply.github.com>
2019-09-17 11:08:24 +0700

    Add files via upload

---
+++ 20190801060_isikubus.js
@@ -0,0 +1,5 @@
+var inkey = require('readline-sync')
+
+var s = parseInt(inkey.question("Masukan Nilai Sisi Kubus : "));
+
+console.log(s*s*s);
---
+++ 20190801060_luassegitiga.js
@@ -0,0 +1,6 @@
+var inkey = require('readline-sync')
+
+var a = parseInt(inkey.question("masukan Nilai Alas : "));
+var t = parseInt(inkey.question("masukan nilai Tinggi : "));
+
+console.log(a*t/2);
