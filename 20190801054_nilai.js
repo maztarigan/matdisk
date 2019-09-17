efc6f99b1f90b8d790c1d58e15d8b9ced098fb91
Thoriq Nurul F <54829616+thoriqnurulfikri2001@users.noreply.github.com>
2019-09-17 12:41:21 +0700

    Add files via upload

---
+++ 20190801054_nilai.js
@@ -0,0 +1,16 @@
+var inputkeyboard = require('readline-sync');
+
+var nilai = parseInt(inputkeyboard.question("masukkan nilai : "));
+
+if ((nilai >= 81) && (nilai <= 100)){
+    console.log("NILAI A");
+}else if ((nilai >= 70) && (nilai <= 80)){
+    console.log("NILAI B");
+}else if ((nilai >= 60) && (nilai <= 69)){
+    console.log("NILAI C")
+}else if ((nilai >= 50) && (nilai <= 59)){
+    console.log("NILAI D")
+}else
+{
+    console.log("NILAI E")
+}
