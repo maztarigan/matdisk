751272343b7af23d7e25b3a9bb2079b09632b434
Mansuru <54829623+mansuru017@users.noreply.github.com>
2019-09-17 11:00:40 +0700

    Add files via upload

---
+++ 20190801050_isikubus.js
@@ -0,0 +1,7 @@
+var rumus = require ('readline-sync');
+
+var s = parseInt(rumus.question("Masukkan Nilai sisi :"));
+var s = parseInt(rumus.question("Masukkan Nilai Sisi :"));
+var s = parseInt(rumus.question("Masukkan Nilai Sisi :"));
+var isi = s*s*s;
+console.log ("isi kubus "+s+" ="+isi);
---
+++ 20190801050_luassegitiga.js
@@ -0,0 +1,6 @@
+var rumus = require ('readline-sync');
+
+var a = parseInt(rumus.question("Masukkan Nilai Alas :"));
+var t = parseInt(rumus.question("Masukkan Nilai Tinggi :"));
+var luas = a*t/2;
+console.log (a*t/2);
