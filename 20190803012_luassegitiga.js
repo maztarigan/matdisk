6586ecc09f93881cdf47e3c4899603204260e117
DitaOktaviani <54826740+DitaOktaviani@users.noreply.github.com>
2019-09-17 11:44:39 +0700

    Add files via upload

---
+++ 20190803012_luassegitiga.js
@@ -0,0 +1,8 @@
+var segitiga = require('readline-sync');
+
+var alas = parseInt(segitiga.question("masukan nilai alas : "));
+var tinggi = parseInt(segitiga.question("mausukan nilai tinggi : "));
+
+var luas = 0.5*alas*tinggi;
+
+console.log(luas);
