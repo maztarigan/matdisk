af6bed9b4e02948e0b2a74730efef383fd18cd4b
farid handoyo <54826083+faridhandoyo-ueu2019@users.noreply.github.com>
2019-09-17 09:59:00 +0700

    Add files via upload

---
+++ 20190801256_isikubus.js
@@ -0,0 +1,6 @@
+var key = require('readline-sync');
+
+var sisi = parseInt(key.question('masukan nilai :'));
+var volume = sisi*sisi*sisi;
+
+console.log(volume);
---
+++ 20190801256_kelilinglingkaran.js
@@ -0,0 +1,7 @@
+var lingkaran = require('readline-sync');
+
+var pi = 3.14;
+var r = parseInt(lingkaran.question('masukan nilai jari-jari :'));
+var keliling = 2*pi*r;
+
+console.log(luas);
