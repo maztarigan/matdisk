5314cd53aa92a91e13f41d00ad0d3fc6e3972190
Andre Ardiansyah <54829724+andre-ard@users.noreply.github.com>
2019-09-17 12:26:49 +0700

    Add files via upload

---
+++ 20190801094_Nilai.js
@@ -0,0 +1,19 @@
+var inputkeyboard = require('readline-sync');
+
+var nilai = parseInt(inputkeyboard.question("Masukkan Nilai : "));
+
+if (nilai > 80){
+    console.log("Nilai A");
+}else if
+    ((nilai > 69) && (nilai < 81)){
+    console.log("Nilai B");
+}else if
+    ((nilai > 59) && (nilai < 70)){
+    console.log("Nilai C");
+}else if
+    ((nilai > 49) && (nilai < 60)){
+    console.log("Nilai D");
+}else 
+{
+    console.log("Nilai E");
+}
