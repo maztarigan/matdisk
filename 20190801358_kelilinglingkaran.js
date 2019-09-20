a3803d2bc309ddca7ae21b80caa3b32ba8d3b4fb
Sindy Ayu Lestari <sindyayulestari69@gmail.com>
2019-09-20 19:36:51 +0700

    Add files via upload

---
+++ 20190801358_kelilinglingkaran.js
@@ -0,0 +1,6 @@
+var masukkandari_keyboard = require('readline-sync');
+var pi = 3.14;
+var r = masukkandari_keyboard.question("masukkan nilai r : ");
+var keliling = 2 * 3.14 * r;
+console.log("keliling lingkaran :"+keliling);
+
