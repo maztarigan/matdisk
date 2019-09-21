da9d5ab6582dd4828893d101bef89d4c75e60c2e
Muhamad Irfan Aldiansyah <55013255+MuhamadIrfan1901@users.noreply.github.com>
2019-09-21 14:30:42 +0700

    Update 20190801353_nilai.js

--- 20190801353_nilai.js
+++ 20190801353_nilai.js
@@ -6,19 +6,19 @@ if (a > 85) {
     console.log ("A");
 }
 
-if ((a > 70) && ( a < 86)) {
+else if ((a > 70) && ( a < 86)) {
     console.log ("B");
 }
 
-if ((a > 60) && ( a < 71)) {
+else if ((a > 60) && ( a < 71)) {
     console.log("C");
 }
 
-if ((a > 50) && (a < 61)) {
+else if ((a > 50) && (a < 61)) {
     console.log("D");
 }
 
-if (a < 51) {
+else if (a < 51) {
     console.log("E");
 }
 
