2d56a67c780d7c327cff0e8fec20256f6eadab90
dimasfadlisaputra <55302223+dimasfadlisaputra@users.noreply.github.com>
2019-09-21 14:07:57 +0700

    Add files via upload

---
+++ 20190801361_nilai.js
@@ -0,0 +1,19 @@
+var baca = require ('readline-sync')
+
+var a = parseInt(baca.question("Masukkan Nilai Bilangan : "))
+
+if(a>85) {
+    console.log ("Nilai anda A");
+} else
+if((a>70) && (a<86)) {
+    console.log ("Nilai anda B");
+} else
+if((a>60) && (a<71)) {
+    console.log ("Nilai anda C");
+} else
+if((a>50) && (a<61)) {
+    console.log ("Nilai anda D");
+} else
+if(51<a) {
+    console.log ("Nilai anda E");
+}
