22b5852dee48da757f4ff27be863798a297d5037
FadhilahSeptiani <55013235+FadhilahSeptiani@users.noreply.github.com>
2019-09-21 14:10:44 +0700

    Add files via upload

---
+++ 20190801367_NILAI.js
@@ -0,0 +1,15 @@
+var baca = require("readline-sync");
+
+var a = parseInt(baca.question("NILAI : "));
+
+if (a > 85){
+    console.log("A");
+} else if ((a > 70) && (a < 86)){
+    console.log("B");
+} else if ((a > 60) && (a < 71)){
+    console.log("C");
+} else if (( a > 50) && (a < 61)){
+    console.log("D");
+} else if (a < 51){
+    console.log("E");
+}
