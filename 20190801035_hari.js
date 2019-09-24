819c3b6bb4c1389e8a9a68e9c8628a9d50910f69
Vika nurliyana <54829767+vikanurliyana@users.noreply.github.com>
2019-09-24 20:01:52 +0700

    Add files via upload

---
+++ 20190801035_hari.js
@@ -0,0 +1,24 @@
+var angka=new Date().getDay();
+switch (angka){
+    case 0:
+        console.log("Hari Minggu");
+        break;
+    case 1:
+        console.log("Hari Senin");
+        break;
+    case 2:
+        console.log("Hari Selasa");
+        break;
+    case 3:
+        console.log("Hari Rabu");
+        break;
+    case 4:
+        console.log("Hari Kamis");
+        break;
+    case 5:
+        console.log("Hari Jum'at");
+        break;
+    case 6:
+        console.log("Hari Sabtu");
+        break;
+}
