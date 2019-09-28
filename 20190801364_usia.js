49d6eff8ca55c79fcd63bb25f1437f826d73b3d4
hapiznuddin <55013162+hapiznuddin@users.noreply.github.com>
2019-09-28 13:22:36 +0700

    Add files via upload

---
+++ 20190801364_usia.js
@@ -0,0 +1,15 @@
+var baca = require ('readline-sync');
+
+var usia = parseInt(baca.question("Masukan Usia : "));
+
+if ((usia > 2) && (usia < 5)) {
+    console.log("PG");
+} else if ((usia  > 4) && (usia < 7)){
+    console.log("TK");
+} else if ((usia > 6) && (usia < 13)){
+    console.log("SD");
+} else if ((usia > 12) && (usia < 16)){
+    console.log("SMP");
+} else if ((usia > 15) && (usia < 19)){
+    console.log("SMA");
+}
