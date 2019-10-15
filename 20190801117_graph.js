73f150689ed45695bc6298abb7249fd54122e418
SyayidAqil <54829660+SYAYID@users.noreply.github.com>
2019-10-15 12:02:04 +0700

    Add files via upload

---
+++ 20190801117_graph.js
@@ -0,0 +1,26 @@
+var inputjarak = require('readline-sync');
+console.log("Data A");
+var jalur_a = [];
+    jalur_a.push(parseInt(inputjarak.question('Jarak A Titik 1 : ')));
+    jalur_a.push(parseInt(inputjarak.question('Jarak A Titik 2 : ')));
+    jalur_a.push(parseInt(inputjarak.question('Jarak A Titik 3 : ')));
+console.log("Data B");
+var jalur_b = [];
+    jalur_b.push(parseInt(inputjarak.question('Jarak B Titik 1 : ')));
+    jalur_b.push(parseInt(inputjarak.question('Jarak B Titik 2 : ')));
+    jalur_b.push(parseInt(inputjarak.question('Jarak B Titik 3 : ')));
+
+        var jarak_jalur_a = jalur_a[0]+jalur_a[1]+jalur_a[2];
+        var jarak_jalur_b = jalur_b[0]+jalur_b[1]+jalur_b[2];
+
+if(jarak_jalur_a>jarak_jalur_b){
+    console.log("---------------")
+    console.log("Jarak Terpanjang : Melewati Jalur A "+jarak_jalur_a+"KM");
+    console.log("Jarak Terdekat : Melewati Jalur B "+jarak_jalur_b+"KM");
+    console.log("---------------")
+}else{
+    console.log("---------------")
+    console.log("Jarak Terpanjang : Melewati Jalur B "+jarak_jalur_b+"KM");
+    console.log("Jarak Terdekat : Melewati Jalur A "+jarak_jalur_a+"KM");
+    console.log("---------------")
+}
