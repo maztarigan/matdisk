5533962bd328252a577e017632a149d1842c4973
Abdul Latif <54829778+Abdullatiff01@users.noreply.github.com>
2019-10-22 12:13:31 +0700

    Add files via upload

---
+++ 20190801036_matriks.js
@@ -0,0 +1,12 @@
+var a =[[2,7],[3,5]]
+var b =[[4,8],[6,9]]
+
+var hasil_1 = a[0][0] + b[0][0]
+var hasil_2 = a[0][1] + b[0][1]
+var hasil_3 = a[1][0] + b[1][0]
+var hasil_4 = a[1][1] + b[1][1]
+
+console.log("penjumlahan nilai matriks: ");
+console.log(a+" di tambah "+b);
+
+console.log(hasil_1, hasil_2, hasil_3, hasil_4);
