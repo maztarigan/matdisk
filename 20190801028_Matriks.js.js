f81c37ed2fbab17d7c23a7ab20ac3bc2e6e70657
Sachrur Ramadhan <54829662+sachrurramadhan@users.noreply.github.com>
2019-10-22 12:00:20 +0700

    Add files via upload

---
+++ 20190801028_Matriks.js.js
@@ -0,0 +1,14 @@
+console.log("Penjumlahan Array: ");
+
+var data_A = [[2,7],[3,5]];
+var data_B = [[4,8],[6,9]];
+
+var hasil_1 = data_A[0][0] + data_B[0][0];
+var hasil_2 = data_A[0][1] + data_B[0][1];
+var hasil_3 = data_A[1][0] + data_B[1][0];
+var hasil_4 = data_A[1][1] + data_B[1][1];
+
+console.log("Penjumlahan Dari Data Array: ");
+console.log(data_A + " Tambah " + data_B);
+
+console.log(hasil_1,hasil_2,hasil_3,hasil_4);
