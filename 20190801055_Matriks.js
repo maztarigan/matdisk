942b8c8ed658ec4c359faf98c75fd173cc7d2233
Ndra Setiawan <54829634+NdraSE2@users.noreply.github.com>
2019-10-22 21:14:15 +0700

    Add files via upload

---
+++ 20190801055_Matriks.js
@@ -0,0 +1,13 @@
+var matriks_A = [[2,5] , [7,3]]
+var matriks_B = [[4,2] , [3,4]]
+var Hasil_1 = matriks_A[0][0] + matriks_B [0][0]
+var Hasil_2 = matriks_A[0][1] + matriks_B [0][1]
+var Hasil_3 = matriks_A[1][0] + matriks_B [1][0]
+var Hasil_4 = matriks_A[1][1] + matriks_B [1][1]
+
+console.log ("Data Array")
+console.log (matriks_A+ "+" +matriks_B)
+console.log ("=========================")
+console.log (Hasil_1, Hasil_2 ) 
+console.log (Hasil_3, Hasil_4 )
+console.log ("=========================")
