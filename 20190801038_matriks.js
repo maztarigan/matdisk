c6fb69df4043985cc9d59bc1b42fb6f53facc850
unggul prayuda <54829758+unggulpray@users.noreply.github.com>
2019-10-22 12:11:23 +0700

    Add files via upload

---
+++ 20190801038_matriks.js
@@ -0,0 +1,12 @@
+var A = [[2,7],[3,5]];
+var B = [[4,8],[6,9]];
+
+var hasil_1 = A[0][0] + B[0][0];
+var hasil_2 = A[0][1] + B[0][1];
+var hasil_3 = A[1][0] + B[1][0];
+var hasil_4 = A[1][1] + B[1][1];
+
+console.log("penjumlahan matriks");
+console.log(A+" Tambah "+ B);
+
+console.log(hasil_1, hasil_2, hasil_3, hasil_4);
