3fe9f9e51372bcdc64ff5863786d2eb85f8717c2
Firlan Prayoga <54829625+fprayog10@users.noreply.github.com>
2019-10-22 12:16:05 +0700

    Add files via upload

---
+++ 20190801120_matriks.js
@@ -0,0 +1,17 @@
+var matriks_a = [[3,5],[2,4]];
+var matriks_b = [[1,3],[4,6]];
+
+var hasil_1 = matriks_a[0][0]+matriks_b[0][0];
+var hasil_2 = matriks_a[0][1]+matriks_b[0][1];
+var hasil_3 = matriks_a[1][0]+matriks_b[1][0];
+var hasil_4 = matriks_a[1][1]+matriks_b[1][1];
+
+console.log("Penjumlahan Matriks");
+console.log("====================");
+console.log("Matriks A");
+console.log(matriks_a);
+console.log("Matriks B");
+console.log(matriks_b);
+console.log("Hasil Dari Matriks A + B = ");
+console.log([hasil_1,hasil_2],[hasil_3,hasil_4]);
+console.log("====================");
