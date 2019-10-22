d6fedeec9d97f670f1b37fe3f5a2831fc6ddbb04
danangabdul <54830216+danangabdul@users.noreply.github.com>
2019-10-22 12:08:25 +0700

    Add files via upload

---
+++ 20190801060_Matriks.js
@@ -0,0 +1,14 @@
+var data_a = [[2,7],[3,5]];
+var data_b = [[4,8],[6,9]];
+
+var hasil_1 = data_a[0][0] + data_b[0][0];
+var hasil_2 = data_a[0][1] + data_b[0][1];
+var hasil_3 = data_a[1][0] + data_b[1][0];
+var hasil_4 = data_a[1][1] + data_b[1][1];
+
+console.log("Penjumlahan Dari Data Array: ");
+console.log(data_a+" Tambah "+data_b);
+
+console.log("HASIL : ");
+console.log(hasil_1,hasil_2,hasil_3,hasil_4);
+console.log("");
