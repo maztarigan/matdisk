61566d6ee1f5ef4225ef16a80dd0b9fa4d1c4727
MickoTubagas <54829600+mickotubagas@users.noreply.github.com>
2019-10-22 12:12:33 +0700

    Add files via upload

---
+++ 20190801086_Matriks.js
@@ -0,0 +1,17 @@
+console.log("Hasil Tambah Matriks A + B");
+console.log("A = 1 2")
+console.log("    3 4")
+console.log("B = 5 6")
+console.log("    7 8")
+var A = [[1,2],[3,4]]
+var B = [[5,6],[7,8]]
+
+var a11 = A[0][0]+B[0][0];
+var a12 = A[0][1]+B[0][1];
+var a21 = A[1][0]+B[1][0];
+var a22 = A[1][1]+B[1][1];
+
+console.log(" hasil matriks a11 = " +a11);
+console.log(" hasil matriks a12 = " +a12);
+console.log(" hasil matriks a21 = " +a21);
+console.log(" hasil matrisk a22 = " +a22);
