8d7ac0bd759cc30927e4fbbef66c76ac163b63a8
Mansuru <54829623+mansuru017@users.noreply.github.com>
2019-10-21 22:22:36 -0700

    Add files via upload

---
+++ 20190801050_matiks.js
@@ -0,0 +1,11 @@
+var a = [[2,7] , [3,5]]
+var b = [[4,8] , [6,9]]
+
+var tambah_1= a[0][0]+b[0][0]
+var tambah_2= a[0][1]+b[0][1]
+var tambah_3= a[1][0]+b[1][0]
+var tambah_4= a[1][1]+b[1][1]
+
+console.log("Penjumlah Array Matriks")
+console.log("["+tambah_1+" "+tambah_2+"]")
+console.log("["+tambah_3+" "+tambah_4+"]")
