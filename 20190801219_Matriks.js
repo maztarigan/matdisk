9e1f71b8d72c6cbbc796beb615697b2e5b657239
pahlevy555 <55126216+pahlevy555@users.noreply.github.com>
2019-10-22 12:19:45 +0700

    Add files via upload

---
+++ 20190801219_Matriks.js
@@ -0,0 +1,19 @@
+var a = [[2,7] , [3,5]]
+var b = [[4,8] , [6,9]]
+
+var tambah1 = a[0][0] + b[0][0] 
+var tambah2 = a[0][1] + b[0][1]
+var tambah3 = a[1][0] + b[1][0]
+var tambah4 = a[1][1] + b[1][1]
+
+console.log ("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+console.log ("       A   :" + "[2  7]" + "       B   :" + "[4  8]")
+console.log ("            " + "[3  5]" + "            " + "[6  9]")
+
+console.log ("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+console.log ("       A+B :" + "[" + tambah1  + " " +  tambah2 + "]")
+console.log ("            " + "[" + tambah3  + " "  + tambah4 + "]")
+
+console.log ("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
