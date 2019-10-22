1887dc7e8947595667f0b5685f2beb5ec5a3934e
Andri Kurnia Maulana <54831901+andrikurnia@users.noreply.github.com>
2019-10-21 22:17:37 -0700

    Add files via upload

---
+++ 20190801056_matriks.js
@@ -0,0 +1,19 @@
+console.log("Matriks A")
+console.log("[2 7]")
+console.log("[3 5]")
+console.log("Matriks B")
+console.log("[4 8]")
+console.log("[6 9]")
+
+var a = [[2,7] , [3,5]]
+var b = [[4,8] , [6,9]]
+
+var tambah_1= a[0][0]+b[0][0]
+var tambah_2= a[0][1]+b[0][1]
+var tambah_3= a[1][0]+b[1][0]
+var tambah_4= a[1][1]+b[1][1]
+
+console.log("Penjumlah Matriks")
+console.log("Matriks A+B")
+console.log("["+tambah_1+" "+tambah_2+"]")
+console.log("["+tambah_3+" "+tambah_4+"]")
