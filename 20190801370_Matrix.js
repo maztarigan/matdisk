76db7827ba5823c36bb71f5f5f762dfa56a49ee7
Jawahir Ilham .R <54989420+jawahirIlhamRamadhan@users.noreply.github.com>
2019-10-25 23:34:41 +0700

    20190801370_Jawahir_Ilham_R

---
+++ 20190801370_Matrix.js
@@ -0,0 +1,15 @@
+console.log("MATRIX");
+
+var DataA = [[2,5],[3,7]];
+var DataB = [[4,9],[6,8]];
+
+var Data1 = DataA[0][0] + DataB[0][0];
+var Data2 = DataA[0][1] + DataB[0][1];
+var Data3 = DataA[1][0] + DataB[1][0];
+var Data4 = DataA[1][1] + DataB[1][1];
+
+console.log("Data A"+"[" + DataA +"]");
+console.log("Data B"+"[" + DataB +"]");
+console.log("Data C = "+ Data1,Data2,Data3,Data4);
+
+console.log("Selesai")  
