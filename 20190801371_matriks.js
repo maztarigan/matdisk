f93b88bcb95aa55177a236cea7e33cf75515de98
Raihan Ghifari <54989469+RaihanGhifari24@users.noreply.github.com>
2019-10-25 23:28:34 +0700

    Add files via upload

---
+++ 20190801371_matriks.js
@@ -0,0 +1,16 @@
+console.log("tugas matriks");
+
+//array
+var DataA = [[2,5],[3,7]];
+var DataB = [[4,9],[6,8]];
+
+//rumusan
+var Data1 = DataA[0][0] + DataB[0][0];
+var Data2 = DataA[0][1] + DataB[0][1];
+var Data3 = DataA[1][0] + DataB[1][0];
+var Data4 = DataA[1][1] + DataB[1][1];
+
+//hasil
+console.log("penjualan dari data array");
+console.log(DataA + "+" + DataB);
+console.log(Data1,Data2,Data3,Data4);
