87e620b28d8e08cb3d80498aadeb363bd27ce46f
Andri Fatur Rochman <55288302+andrifaturrochman@users.noreply.github.com>
2019-10-25 23:54:58 +0700

    20190801356_ANDRI_FATUR_ROCHMAN

---
+++ 20190801356_matrix.js
@@ -0,0 +1,15 @@
+console.log("WILU JEUNG SUMPING SADAYANA");
+
+var A = [[2,5],[3,7]];
+var B = [[4,9],[6,8]];
+
+var C1 = A [0][0] + B[0][0];
+var C2 = A [0][1] + B[0][1];
+var C3 = A [1][0] + B[1][0];
+var C4 = A [1][1] + B[1][1];
+
+console.log("A = " + "[" + A + "]");
+console.log("B = " + "[" + B + "]");
+console.log("C = "+"["+(C1),(C2),(C3),(C4)+"]");
+
+console.log("HATUR NUHUN SADAYA");
