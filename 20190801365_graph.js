cf3a7efb8d53f54606ad84d0e392a633caa75d5b
Sutna Wijaya <54989490+sutna@users.noreply.github.com>
2019-10-26 00:02:19 +0700

    graph

---
+++ 20190801365_graph.js
@@ -0,0 +1,14 @@
+console.log("welcome to my data");
+
+var angka_A = [[2,5] , [3,7]];
+var angka_B = [[4,9] , [6,8]];
+
+var nomor_A = angka_A[0][0] + angka_B[0][0];
+var nomor_B = angka_A[0][1] + angka_B[0][1];
+var nomor_C = angka_A[1][0] + angka_B[1][0];
+var nomor_D = angka_A[1][1] + angka_B[1][1];
+
+console.log("cetak hasil data C : ");
+console.log(nomor_A,nomor_B,nomor_C,nomor_D);
+
+console.log("thaks you");
