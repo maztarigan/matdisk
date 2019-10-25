d5f7c7732cdb427d31f9595220e02c664e5cc3b7
ALFIAN AJI WAHYUDI <45096863+alfianaji24@users.noreply.github.com>
2019-10-25 23:51:05 +0700

    Add files via upload

---
+++ 20190801422_Matriks.js
@@ -0,0 +1,21 @@
+console.log('WELCOME');
+
+//array
+var A = [[2,5],[3,7]];
+var B = [[4,9],[6,8]];
+
+//Rumusan
+var Sub1 = A[0][0] + B[0][0];
+var Sub2 = A[0][1] + B[0][1];
+var Sub3 = A[1][0] + B[1][0];
+var Sub4 = A[1][1] + B[1][1];
+
+//Hasil
+console.log("C = ");
+//Data A
+console.log("A : " + "[" + A +"]");
+//Data B
+console.log("B : " + "[" + B + "]");
+console.log(Sub1,Sub2,Sub3,Sub4);
+
+console.log('TERIMA KASIH');
