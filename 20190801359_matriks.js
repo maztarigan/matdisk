745a9c5df3efb43a4d4027b7e17f7a917681f7ef
Anrian sihotang <54989405+anriansihotang@users.noreply.github.com>
2019-10-25 23:29:26 +0700

    Add files via upload

---
+++ 20190801359_matriks.js
@@ -0,0 +1,13 @@
+console.log("welcome");
+
+var A = [[2,5],[3,7]];
+var B = [[4,9],[6,8]];
+
+var data_1 = A[0][0]+B[0][0];
+var data_2 = A[0][1]+B[0][1];
+var data_3 = A[1][0]+B[1][0];
+var data_4 = A[1][1]+B[1][1];
+console.log("c : ");
+console.log(data_1,data_2,data_3,data_4);
+
+console.log("terima kasih")
