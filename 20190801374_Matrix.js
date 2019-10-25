019bd82ad4110a934a2ac3c4f18494d4945e0273
Lorenzo Padang <54989407+LorenzoPadang@users.noreply.github.com>
2019-10-25 23:43:08 +0700

    Add files via upload

---
+++ 20190801374_Matrix.js
@@ -0,0 +1,18 @@
+console.log("Welcome");
+
+
+var A = [[2,5],[3,7]];
+var B = [[4,9],[6,8]];
+
+var C1 = A[0][0] + B[0][0];
+var C2 = A[0][1] + B[0][1];
+var C3 = A[1][0] + B[1][0];
+var C4 = A[1][1] + B[1][1];
+
+
+console.log("Data A" + "[" + A + "]");
+console.log("Data B" + "[" + B + "]");
+console.log("C = "); 
+console.log(C1,C2,C3,C4);
+
+console.log("Thank you");
