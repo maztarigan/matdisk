dda5bfd39b4573453208b208de306b34be3680f0
Kresna Ramadhan <dhanns777@gmail.com>
2019-10-25 23:45:05 +0700

    Add files via upload

---
+++ 20190801375_Matriks.js
@@ -0,0 +1,30 @@
+//Welcome
+console.log("Selamat Datang :)");
+console.log("Ini adalah penjumlahan Matriks Ordo 2x2..");
+
+//Pernyataan Matriks
+var matriksA = [[2,5],[3,7]];
+var matriksB = [[4,9],[6,8]];
+
+//Rumus
+var res1 = matriksA[0][0] + matriksB[0][0];
+var res2 = matriksA[0][1] + matriksB[0][1];
+var res3 = matriksA[1][0] + matriksB[1][0];
+var res4 = matriksA[1][1] + matriksB[1][1];
+
+//Penjumlahan Matriks
+console.log("Jadi Hasil Penjumlahan dari Matriks =>");
+console.log(matriksA+" dan "+matriksB+" adalah =>");
+
+//Hasil atau Result
+console.log("============================");
+console.log(" |( "+res1+" "+res2+" "+res3+" "+res4+" )| ");
+console.log("============================");
+console.log("atau");
+console.log("============================");
+console.log(res1+" "+res2);
+console.log(res3+" "+res4);
+console.log("=============================");
+
+//Thanks
+console.log("Terimakasih :) || @Nra.375");
