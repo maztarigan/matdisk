aa47c9b3cd4c7ab8def30e8dacf912d7ed9c49c0
Alfikry21 <54989562+Alfikry21@users.noreply.github.com>
2019-10-25 23:29:08 +0700

    Add files via upload

---
+++ 20190801417_Matrix.js
@@ -0,0 +1,13 @@
+var data_A = [[2,5],[3,7]];
+var data_B = [[4,9],[6,8]];
+
+var data_1 = data_A[0][0] + data_B[0][0];
+var data_2 = data_A[0][1] + data_B[0][1];
+var data_3 = data_A[1][0] + data_B[1][0];
+var data_4 = data_A[1][1] + data_B[1][1];
+
+//data A
+console.log("data A" + "[" + data_A +"]");
+//data B
+console.log("data B" + "[" + data_B +"]");
+console.log("data C = " + data_1, data_2, data_3, data_4);
