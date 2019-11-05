7d5e01026cd7e1913721df0b6e7549e0744f7433
Danu Faisal Pangestu <54826607+danufaisalpangestu05@users.noreply.github.com>
2019-11-05 10:05:32 +0700

    Add files via upload

---
+++ 20190803077_octa.js
@@ -0,0 +1,9 @@
+console.log("Danu Faisal Pangestu");
+console.log(" NIM : 20190803077");
+console.log("\n");
+
+var key = require('readline-sync');
+var desimal = parseInt(key.question("masukan nilai Desimal :"));
+
+console.log("hasil convert dari decimal ke octa");
+console.log(desimal.toString(8));
