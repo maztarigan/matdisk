734f4fd621f8ff68482171a479b6340bc6b8338e
ndarulanggeng244 <52662930+ndarulanggeng244@users.noreply.github.com>
2019-11-05 09:52:58 +0700

    Add files via upload

---
+++ 20190803036_biner.js
@@ -0,0 +1,7 @@
+var key = require("readline-sync");
+
+var desimal = parseInt(key.question('Masukan Nilai Desimal : '));
+var desimal = (desimal, 10)
+var biner = (desimal.toString(2));
+
+console.log(biner);
