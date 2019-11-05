094d893fb20c6fa6e1dbb91d78aba0962c936c8a
iva sanggra <54826686+ivasanggra@users.noreply.github.com>
2019-11-05 10:19:30 +0700

    Add files via upload

---
+++ 20190803032_binner.js
@@ -0,0 +1,5 @@
+var key = require('readline-sync');
+var desimal = parseInt(key.question("masukan nilai desimal :"));
+
+console.log("hasil");
+console.log(desimal.toString(2));
---
+++ 20190803032_okta.js
@@ -0,0 +1,5 @@
+var key = require('readline-sync');
+var desimal = parseInt(key.question("masukan nilai okta :"));
+
+console.log("hasil");
+console.log(desimal.toString(8));
