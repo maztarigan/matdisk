8bbd087b5a8e8827fcf3d067209c80cd0b15e676
diki anugrah <54826339+dikianugrah-ueu2019@users.noreply.github.com>
2019-11-05 09:56:02 +0700

    Add files via upload

---
+++ Diki_20190801313_octa.js
@@ -0,0 +1,10 @@
+console.log("Diki Anugrah");
+console.log("20190801313");
+console.log("\n");
+
+var key = require("readline-sync");
+var desimal = parseInt(key.question(" Masukan Nilai Desimal :"));
+
+console.log("hasil");
+console.log(desimal.toString(8));
+
