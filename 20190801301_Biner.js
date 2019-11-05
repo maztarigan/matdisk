cf72021d865b70e73c08b312c533ca6a20219054
sendihot sihole <54826481+sendihotsiholeueu2019@users.noreply.github.com>
2019-11-05 09:56:27 +0700

    Add files via upload

---
+++ 20190801301_Biner.js
@@ -0,0 +1,5 @@
+var key = require("readline-sync");
+
+var desimal = parseInt(key.question('masukkan nilai desimal :'));
+
+console.log ("Biner = "+desimal.toString(2));
---
+++ 20190801301_Hexa.js
@@ -0,0 +1,5 @@
+var key = require("readline-sync");
+
+var desimal = parseInt(key.question('masukkan nilai desimal :'));
+
+console.log ("Hexa = "+desimal.toString(16));
