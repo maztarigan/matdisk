7e842732ba1ad57cdf7172a87dcf1a4574a839bf
Vika nurliyana <54829767+vikanurliyana@users.noreply.github.com>
2019-11-05 12:07:16 +0700

    Add files via upload

---
+++ 20190801035_biner.js
@@ -0,0 +1,15 @@
+//Desimal to Biner
+
+
+var input = require("readline-sync");
+
+var desimal = parseInt(input.question("Masukkan Nilai Desimal : "));
+var octa    = parseInt(input.question("Masukkan Nilai Octa :"));
+var hexa    = parseInt(input.question("Masukkan Nilai Hexadecimal :"))
+var biner   = (desimal.toString(2));
+var biner_1 = (octa.toString(8));
+var biner_2 = (hexa.toString(16));
+
+console.log("Hasil Biner ke Decimal adalah : ["+biner+"]");
+console.log("Hasil Biner ke Octa adalah : ["+biner_1+"]");
+console.log("Hasil Biner ke Hexadecimal adalah : ["+biner_2+"]");
---
+++ 20190801035_hexa.js
@@ -0,0 +1,7 @@
+var key = require("readline-sync");
+
+var desimal = parseInt(key.question('Masukan Nilai Desimal : '));
+var hexa = (desimal.toString(16));
+
+console.log("====================================================================");
+console.log(hexa);
