ef44f4bf1eb0d63e38ec5da16cd0faf227d05b66
ndarulanggeng244 <52662930+ndarulanggeng244@users.noreply.github.com>
2019-11-05 10:04:43 +0700

    Update 20190803036_octa.js

--- 20190803036_octa.js
+++ 20190803036_octa.js
@@ -1,6 +1,6 @@
 var key = require("readline-sync");
 
 var desimal = parseInt(key.question('Masukan Nilai Desimal : '));
-var biner = (desimal.toString(8));
+var octa = (desimal.toString(8));
 
-console.log(biner);
+console.log(octa);
