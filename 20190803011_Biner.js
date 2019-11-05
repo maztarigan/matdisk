d06157b15718b48fd763edf1d4899a97108b5fd8
Indah Cahyani <54826941+Indahcahyanii@users.noreply.github.com>
2019-11-05 10:19:09 +0700

    Add files via upload

---
+++ 20190803011_Biner.js
@@ -0,0 +1,9 @@
+console.log("Indah Cahyani");
+console.log("NIM : 20190803011");
+console.log("\n");
+
+var key = require('readline-sync');
+var desimal = parseInt(key.question("masukkan nilai desimal :"));
+
+console.log("hasil");
+console.log(desimal.toString(2));
---
+++ 20190803011_Octa.js
@@ -0,0 +1,9 @@
+console.log("Indah Cahyani");
+console.log("NIM : 20190803011");
+console.log("\n");
+
+var key = require('readline-sync');
+var octa = parseInt(key.question("masukkan nilai octa :"));
+
+console.log("hasil");
+console.log(octa.toString(8));
