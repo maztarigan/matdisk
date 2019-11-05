9c375bcf3f148ecf7953a5a28659dc7470baffbd
farid handoyo <54826083+faridhandoyo-ueu2019@users.noreply.github.com>
2019-11-05 10:06:44 +0700

    Add files via upload

---
+++ 20190801256_octa.js
@@ -0,0 +1,7 @@
+var key = require("readline-sync");
+var desimal = parseInt(key.question(" Masukan Nilai Desimal :"));
+
+console.log("\n")
+console.log("hasil convert dari decimal ke octa");
+console.log(desimal.toString(8));
+
