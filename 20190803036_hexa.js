4744dcb9e6afb30fa1e29896d3b4d40f6397ad83
ndarulanggeng244 <52662930+ndarulanggeng244@users.noreply.github.com>
2019-11-05 10:01:55 +0700

    Update 20190803036_hexa.js

--- 20190803036_hexa.js
+++ 20190803036_hexa.js
@@ -1,7 +1,6 @@
 var key = require("readline-sync");
 
 var desimal = parseInt(key.question('Masukan Nilai Desimal : '));
-var desimal = (desimal, 10)
 var biner = (desimal.toString(16));
 
 console.log(biner);
