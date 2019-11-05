f5e091a012db4edd14a1ddecdca744e0b95592fa
khairul fadhly <54829667+khairulfadhly@users.noreply.github.com>
2019-11-05 11:44:11 +0700

    Add files via upload

---
+++ 20190801085_biner.js
@@ -0,0 +1,8 @@
+
+var key = require("readline-sync");
+
+var desimal = parseInt(key.question('Masukan Nilai Desimal : '));
+var biner = (desimal.toString(2));
+
+console.log("===================================================================");
+console.log(biner);
