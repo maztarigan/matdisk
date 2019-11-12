0cc7090fb77b8cb54b1e6e4bf9ef3df11856fb86
farid handoyo <54826083+faridhandoyo-ueu2019@users.noreply.github.com>
2019-11-13 04:48:58 +0700

    Add files via upload

---
+++ 20190801256_soal2.js
@@ -0,0 +1,18 @@
+var key = require("readline-sync");
+var x = ""
+var n = parseInt(key.question('Input Nilai : '));
+
+console.log("------------ SOAL 2 --------------")
+console.log("\n\n")
+for(var i=1; i<=n; i++){
+    for(var j = 1; j<=n; j++){
+        if(i + j <= n && i>= j){
+            x += "* "
+        }else{
+            x += " "
+        }
+    }
+    x += "\n"
+}
+console.log(x)
+console.log("------------------------------------------")
