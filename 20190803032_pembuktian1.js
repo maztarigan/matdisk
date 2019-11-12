85098548cb90644627e2dd0de74b507437f7dbbb
iva sanggra <54826686+ivasanggra@users.noreply.github.com>
2019-11-12 19:03:27 +0700

    Add files via upload

---
+++ 20190803032_pembuktian1.js
@@ -0,0 +1,17 @@
+var key = require("readline-sync");
+var x = ""
+var n = parseInt(key.question('Input Nilai : '));
+
+for(var a = n; a > 0; a--){
+    for(var i= 1; i <= a; i++){
+        x += " "
+    }
+    for(var a1 = n; a1 > a; a1--){
+        x +="*"
+    }
+    for(var a2 = n-1; a2 > a; a2--){
+        x +="*"
+    }
+    console.log(x);
+    x= ""
+}
