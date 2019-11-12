9f90c921f48cd9c2da59ebf28167b07fcca3cb0e
MickoTubagas <54829600+mickotubagas@users.noreply.github.com>
2019-11-12 13:00:43 +0700

    Add files via upload

---
+++ 20190801086_soal2.js
@@ -0,0 +1,20 @@
+var key, n
+key = require('readline-sync')
+n   = parseInt(key.question("Masukkan Nilai = "))
+
+
+var str = "";
+
+for(var i = 1; i < n; i++ ){
+    for(var j = 1; j < n; j++){
+        if(i + j >= n && i <= j){
+            str = str.concat("*");
+        }else{
+            str = str.concat(" ")
+        }
+    }
+    str = str.concat("\n")
+}
+console.log(str)
+console.log("========================");
+console.log("by : Micko Tubagas");
