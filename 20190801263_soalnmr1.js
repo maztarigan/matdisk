17e1666f8e9a35ff994e4578fddde729e841819b
Sephiaanandaputri <55426634+sephiaananda@users.noreply.github.com>
2019-11-12 18:53:40 +0700

    Add files via upload

---
+++ 20190801263_soalnmr1.js
@@ -0,0 +1,18 @@
+var key = require('readline-sync');
+
+var y = parseInt(key.question('Input Angka : '));
+
+
+            let str = "";
+
+            for(let i = 1; i < y; i++ ){
+                for(let j = 1; j < y; j++){
+                    if(i + j >= y && i >= j){
+                        str = str.concat("*");
+                    }else{
+                        str = str.concat(" ")
+                    }
+                }
+                str = str.concat("\n")
+            }
+            console.log(str)
