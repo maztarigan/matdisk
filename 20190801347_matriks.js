0968ce180f3ecb1c4e00eca876a09a50ae131931
Setianing Budi <55282999+setianing-budi@users.noreply.github.com>
2019-11-15 00:04:52 +0700

    Add files via upload

---
+++ 20190801347_matriks.js
@@ -0,0 +1,47 @@
+console.log("Menentukan Waktu dan Jarak Tempuh Array 2 Dimensi")
+
+var jalur_A = [[5,30], [10,5], [2,10]]
+var jalur_B = [[5,30], [6,45], [3,20]]
+
+var jarak_jalur_A = jalur_A[0][0]+jalur_A[1][0]+jalur_A[2][0]
+var jarak_jalur_B = jalur_B[0][0]+jalur_B[1][0]+jalur_B[2][0]
+
+var waktu_jalur_A = jalur_A[0][1]+jalur_A[1][1]+jalur_A[2][1]
+var waktu_jalur_B = jalur_B[0][1]+jalur_B[1][1]+jalur_B[2][1]
+
+var inputkeyboard = require('readline-sync')
+
+console.log("►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄")
+var jalur_A1 = parseInt(inputkeyboard.question('Jarak Jalur A1 : '))
+var jalur_A2 = parseInt(inputkeyboard.question('Jarak Jalur A2 : '))
+var jalur_A3 = parseInt(inputkeyboard.question('Jarak Jalur A3 : '))
+console.log("►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄")
+var jalur_B1 = parseInt(inputkeyboard.question('Jarak Jalur B1 : '))
+var jalur_B2 = parseInt(inputkeyboard.question('Jarak Jalur B2 : '))
+var jalur_B3 = parseInt(inputkeyboard.question('Jarak Jalur B3 : '))
+console.log("====================")
+var waktu_jalur_A1 = parseInt(inputkeyboard.question('Waktu Tempuh Jalur A1 : '))
+var waktu_jalur_A2 = parseInt(inputkeyboard.question('Waktu Tempuh Jalur A2 : '))
+var waktu_jalur_A3 = parseInt(inputkeyboard.question('Waktu Tempuh Jalur A3 : '))
+console.log("►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄")
+var waktu_jalur_B1 = parseInt(inputkeyboard.question('Waktu Tempuh Jalur B1 : '))
+var waktu_jalur_B2 = parseInt(inputkeyboard.question('Waktu Tempuh Jalur B2 : '))
+var waktu_jalur_B3 = parseInt(inputkeyboard.question('Waktu Tempuh Jalur B3 : '))
+console.log("►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄")
+
+if (jarak_jalur_A > jarak_jalur_B && waktu_jalur_A > waktu_jalur_B){
+    console.log("►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄")
+    console.log("Jarak Terpanjang : "+jarak_jalur_B+ "KM")
+    console.log("Waktu Tempuh Tercepat : "+waktu_jalur_B+ "Menit")
+    console.log("Jarak Terpendek : "+jarak_jalur_A+"KM")
+    console.log("Waktu Tempuh Terlama : "+waktu_jalur_A+ "Menit")
+    console.log("►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄")
+}
+else{
+    console.log("►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄")
+    console.log("Jarak Terpanjang : "+jarak_jalur_A+ "KM")
+    console.log("Waktu Tempuh Tercepat : "+waktu_jalur_A+ "Menit")
+    console.log("Jarak Terpendek : "+jarak_jalur_B+ "KM")
+    console.log("Waktu Tempuh Terlama : "+waktu_jalur_B+ "Menit")
+    console.log("►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄")
+}
---
+++ 20190801347_penjumlahanmatriks.js
@@ -0,0 +1,25 @@
+console.log("Penjumlahan Matriks")
+
+var matriks_A = [[3,4,5], [2,3,8]]
+var matriks_B = [[7,9,10], [3,6,4]]
+
+console.log("====================")
+console.log("A = "+"|"+3, 4, 5+"|")
+console.log("    "+"|"+2, 3, 8+"|","\n")
+
+console.log("B = "+"|"+7, 9, 10+"|")
+console.log("    "+"|"+3, 6, 4+"|")
+
+//PENJUMLAHAN MATRIKS
+var Hasil_1 = matriks_A [0][0] + matriks_B [0][0]
+var Hasil_2 = matriks_A [0][1] + matriks_B [0][1]
+var Hasil_3 = matriks_A [0][2] + matriks_B [0][2]
+var Hasil_4 = matriks_A [1][0] + matriks_B [1][0]
+var Hasil_5 = matriks_A [1][1] + matriks_B [1][1]
+var Hasil_6 = matriks_A [1][2] + matriks_B [1][2]
+
+console.log("====================","\n")
+console.log("►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄")
+console.log("A + B = "+"|" +Hasil_1, Hasil_2, Hasil_3+"|")
+console.log("        "+"|" +Hasil_4, Hasil_5, Hasil_6+"|")
+console.log("►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄")
