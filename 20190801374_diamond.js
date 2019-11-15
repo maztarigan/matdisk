98fa9c6c562b1b278dd771b1ae91162d82bb61b8
Lorenzo Padang <54989407+LorenzoPadang@users.noreply.github.com>
2019-11-16 00:24:19 +0700

    Valent's_Lorenzo_Padang

---
+++ 20190801374_diamond.js
@@ -0,0 +1,30 @@
+console.log("Welcome");
+console.log("Tugas Matdis Diamond")
+var key = require("readline-sync");
+var x = ""
+var n = parseInt(key.question('Input Nilai : '));
+console.log("======================================");
+var a = '';
+for(var a=n; a>0; a--){
+    for(var i=1; i<=a; i++){
+        x += "  ";
+    }
+    for(var a1=n; a1>a; a1--){
+        x += "*"+"   ";
+    }
+    console.log(x);
+    x= "";
+}
+var x = '';
+for(var i=n; i>=1; i--){
+    for(var k=n; k>i; k--){
+        x += "  ";
+    }
+    for(var j=i; j>=1; j--){
+        x += "*"+"   ";
+    }
+    console.log(x);
+    x = "";
+}
+console.log("======================================");
+console.log("Thank you");
