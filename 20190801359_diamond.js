5c5a2761395cb1278ae16168c1caf48836569ec4
Anrian sihotang <54989405+anriansihotang@users.noreply.github.com>
2019-11-16 22:05:02 +0700

    Add files via upload

---
+++ 20190801359_diamond.js
@@ -0,0 +1,27 @@
+console.log("Nama : Anrian Sihotang")
+console.log("NIM : 20190801359")
+var key = require("readline-sync");
+var x = ""
+var n = parseInt(key.question('Input Nilai : '));
+var a = '';
+for(var a=n; a>0; a--){
+    for(var i=1; i<=a; i++){
+        x += "  ";
+    }
+    for(var a1=n; a1>a; a1--){
+        x += "*"+"   ";
+    }
+    console.log(x);
+    x= "";
+}
+var x = '';
+for(var i=n; i>=1; i--){
+    for(var k=n; k>i; k--){
+        x += "  ";
+    }
+    for(var j=i; j>=1; j--){
+        x += "*"+"   ";
+    }
+    console.log(x);
+    x = "";
+}
