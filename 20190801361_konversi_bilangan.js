5c03887fd384b991c5051c1da9917e6bf2e23059
dimasfadlisaputra <55302223+dimasfadlisaputra@users.noreply.github.com>
2019-11-16 14:09:55 +0700

    Add files via upload

---
+++ 20190801361_konversi_bilangan.js
@@ -0,0 +1,19 @@
+var input = require("readline-sync");
+
+console.log("========================================================");
+console.log("Konversi Bilangan Dari Desimal Ke Biner,Octa,Hexadesimal");
+console.log("========================================================");
+
+var desimal = parseInt(input.question("Masukkan Nilai Desimal : "));
+
+var biner = (desimal.toString(2));
+var biner_1 = (desimal.toString(8));
+var biner_2 = (desimal.toString(16));
+
+console.log("Hasil Desimal ke Biner Adalah : ["+biner+"]");
+console.log("Hasil Desimal ke Octa Adalah : ["+biner_1+"]" );
+console.log("Hasil Desimal ke Hexadesimal Adalah : ["+biner_2+"]");
+
+console.log("===================================");
+console.log("By Dimas Fadli Saputra, 20190801361");
+console.log("===================================");
