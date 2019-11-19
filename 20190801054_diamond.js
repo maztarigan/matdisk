be633a92c41b9e901ce872b288cfa4a7df4f1c64
Thoriq Nurul F <54829616+thoriqnurulfikri2001@users.noreply.github.com>
2019-11-19 12:53:36 +0700

    Add files via upload

---
+++ 20190801054_diamond.js
@@ -0,0 +1,28 @@
+console.log("Thoriq Nurul Fikri");
+console.log("Diamond");
+console.log("20190801054");
+var key, n
+key = require('readline-sync')
+n   = parseInt(key.question("Masukkan n! = "))
+
+    var x = '';
+    for(var i=n; i>=1; i--){
+        for(var k=i; k>=1; k--){
+            x += " ";
+        }
+        for(var j=n; j>=i; j--){
+            x += "*"+" ";
+        }
+        console.log(x);
+        x = "";
+    }
+    for(var i=n-1; i>=1; i--){
+        for(var k=n; k>=i; k--){
+            x += " ";
+        }
+        for(var j=i; j>=1; j--){
+            x += "*"+" ";
+        }
+        console.log(x);
+        x = "";
+    }
