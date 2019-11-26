bab9c76fe9fb5b80d4463ff739bbb4a9aabc8a53
Kevin Semuel Yuri M <54829729+kevinsym@users.noreply.github.com>
2019-11-26 21:49:34 +0700

    Add files via upload

---
+++ 20190801052_JarakdanWaktu.js
@@ -0,0 +1,212 @@
+var iyay = [18,29]
+var firlan= [7,15]
+var tomy = [16,35]
+var magfur = [6,10]
+var jeje = [10,25]
+var kevin = [2,5]
+var andri = [8,22]
+var syayid= [18,32]
+var latif= [18,35]
+var thoriq= [6.2,17]
+var andre = [30,90]
+var unggul= [16,45]
+var micko= [1,15]
+var mansur= [8.4,21]
+var levy= [15.8,21.8]
+var fadhly= [12,25]
+var rob= [1,3]
+var indra= [14,45]
+var dallas= [0.5 , 2]
+var diar= [3.9,11]
+var chandra= [17,45]
+var dana= [2.5 , 15]
+var vika= [5.1 , 20]
+var dimas= [15,33]
+var danang= [1,5]
+var sachrur= [16,31]
+
+
+var jarak  = [[iyay [0]]    , [firlan [0]] , [tomy [0]]   , [magfur [0]],
+              [jeje [0]]    , [kevin [0]]  , [andri [0]]  , [syayid [0]],
+              [latif [0]]   , [thoriq [0]] , [andre [0]]  , [unggul [0]],
+              [micko [0]]   , [mansur [0]] , [levy [0]]   , [fadhly [0]],
+              [rob [0]]     , [indra [0]]  , [dallas [0]] , [diar [0]],
+              [chandra [0]] , [dana [0]]   , [vika [0]]   , [dimas [0]],
+              [danang [0]]  , [sachrur [0]]]
+
+
+
+var max = Math.max.apply(null,jarak )
+switch(max){
+    case iyay[0]:
+    console.log('jarak  terjauh adalah Iyay dengan jarak  ' + max)
+    break;
+    case firlan[0]:
+    console.log('jarak  terjauh adalah Firlan dengan jarak  ' + max)
+    break;
+    case tomy[0]:
+    console.log('jarak  terjauh adalah Tomy dengan jarak  ' + max)
+    break;
+    case magfur[0]:
+    console.log('jarak  terjauh adalah Magfur dengan jarak  ' + max)
+    break;
+    case jeje[0]:
+    console.log('jarak  terjauh adalah Jeje dengan jarak  ' + max)
+    break;
+    case kevin[0]:
+    console.log('jarak  terjauh adalah Kevin dengan jarak  ' + max)
+    break;
+    case andri[0]:
+    console.log('jarak  terjauh adalah Andri dengan jarak  ' + max)
+    break;
+    case syayid[0]:
+    console.log('jarak  terjauh adalah Syayid dengan jarak  ' + max)
+    break;
+    case latif[0]:
+    console.log('jarak  terjauh adalah Latif dengan jarak  ' + max)
+    break;
+    case thoriq[0]:
+    console.log('jarak  terjauh adalah Thoriq dengan jarak  ' + max)
+    break;
+    case andre[0]:
+    console.log('jarak  terjauh adalah Andre dengan jarak  ' + max)
+    break;
+    case unggul[0]:
+    console.log('jarak  terjauh adalah Unggul dengan jarak  ' + max)
+    break;
+    case micko[0]:
+    console.log('jarak  terjauh adalah Micko dengan jarak  ' + max)
+    break;
+    case mansur[0]:
+    console.log('jarak  terjauh adalah Mansur dengan jarak  ' + max)
+    break;
+    case levy[0]:
+    console.log('jarak  terjauh adalah Levy dengan jarak  ' + max)
+    break;
+    case fadhly[0]:
+    console.log('jarak  terjauh adalah Fadhly dengan jarak  ' + max)
+    break;
+    case rob[0]:
+    console.log('jarak  terjauh adalah Rob dengan jarak  ' + max)
+    break;
+    case indra[0]:
+    console.log('jarak  terjauh adalah Indra dengan jarak  ' + max)
+    break;
+    case dallas[0]:
+    console.log('jarak  terjauh adalah Dallas dengan jarak  ' + max)
+    break;
+    case diar[0]:
+    console.log('jarak  terjauh adalah Diar dengan jarak  ' + max)
+    break;
+    case chandra[0]:
+    console.log('jarak  terjauh adalah Dhandra dengan jarak  ' + max)
+    break;
+    case dana[0]:
+    console.log('jarak  terjauh adalah Dana dengan jarak  ' + max)
+    break;
+    case vika[0]:
+    console.log('jarak  terjauh adalah Dika dengan jarak  ' + max)
+    break;
+    case dimas[0]:
+    console.log('jarak  terjauh adalah Dimas dengan jarak  ' + max)
+    break;
+    case danang[0]:
+    console.log('jarak  terjauh adalah Danang dengan jarak  ' + max)
+    break;
+    case sachrur[0]:
+    console.log('jarak  terjauh adalah Sachrur dengan jarak ' + max)
+    break;
+    
+}
+ 
+console.log("===================================================================")
+var waktu = [[iyay [1]]    , [firlan [1]] , [tomy [1]]   , [magfur [1]],
+             [jeje [1]]    , [kevin [1]]  , [andri [1]]  , [syayid [1]],
+             [latif [1]]   , [thoriq [1]] , [andre [1]]  , [unggul [1]],
+             [micko [1]]   , [mansur [1]] , [levy [1]]   , [fadhly [1]],
+             [rob [1]]     , [indra [1]]  , [dallas [1]] , [diar [1]],
+             [chandra [1]] , [dana [1]]   , [vika [1]]   , [dimas [1]],
+             [danang [1]]  , [sachrur [1]]]
+
+var min =  Math.min.apply(null,waktu)
+
+switch(min){
+    case iyay[1]:
+    console.log('Waktu tercepat adalah Iyay dengan waktu' + min)
+    break;
+    case firlan[1]:
+    console.log('Waktu tercepat adalah Firlan dengan waktu' + min)
+    break;
+    case tomy[1]:
+    console.log('Waktu tercepat adalah Tomy dengan waktu' + min)
+    break;
+    case magfur[1]:
+    console.log('Waktu tercepat adalah Magfur dengan waktu' + min)
+    break;
+    case jeje[1]:
+    console.log('Waktu tercepat adalah Jeje dengan waktu' + min)
+    break;
+    case kevin[1]:
+    console.log('Waktu tercepat adalah Kevin dengan waktu' + min)
+    break;
+    case andri[1]:
+    console.log('Waktu tercepat adalah Andri dengan waktu' + min)
+    break;
+    case syayid[1]:
+    console.log('Waktu tercepat adalah Syayid dengan waktu' + min)
+    break;
+    case latif[1]:
+    console.log('Waktu tercepat adalah Latif dengan waktu' + min)
+    break;
+    case thoriq[1]:
+    console.log('Waktu tercepat adalah Thoriq dengan waktu' + min)
+    break;
+    case andre[1]:
+    console.log('Waktu tercepat adalah Andre dengan waktu' + min)
+    break;
+    case unggul[1]:
+    console.log('Waktu tercepat adalah Unggul dengan waktu' + min)
+    break;
+    case micko[1]:
+    console.log('Waktu tercepat adalah Micko dengan waktu' + min)
+    break;
+    case mansur[1]:
+    console.log('Waktu tercepat adalah Mansur dengan waktu' + min)
+    break;
+    case levy[1]:
+    console.log('Waktu tercepat adalah Levy dengan waktu' + min)
+    break;
+    case fadhly[1]:
+    console.log('Waktu tercepat adalah Fadhly dengan waktu' + min)
+    break;
+    case rob[1]:
+    console.log('Waktu tercepat adalah Rob dengan waktu' + min)
+    break;
+    case indra[1]:
+    console.log('Waktu tercepat adalah Indra dengan waktu' + min)
+    break;
+    case dallas[1]:
+    console.log('Waktu tercepat adalah Dallas dengan waktu ' + min)
+    break;
+    case diar[1]:
+    console.log('Waktu tercepat adalah Diar dengan waktu' + min)
+    break;
+    case chandra[1]:
+    console.log('Waktu tercepat adalah Chandra dengan waktu' + min)
+    break;
+    case dana[1]:
+    console.log('Waktu tercepat adalah Dana dengan waktu' + min)
+    break;
+    case vika[1]:
+    console.log('Waktu tercepat adalah Vika dengan waktu' + min)
+    break;
+    case dimas[1]:
+    console.log('Waktu tercepat adalah Dimas dengan waktu' + min)
+    break;
+    case danang[1]:
+    console.log('Waktu tercepat adalah Danang dengan waktu' + min)
+    break;
+    case sachrur[1]:
+    console.log('Waktu tercepat adalah Sachrur dengan waktu' + min)
+    break;
+}
