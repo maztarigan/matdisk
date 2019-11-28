30325e62e33d5164b419fd55d3e734ede7ffd241
Setianing Budi <55282999+setianing-budi@users.noreply.github.com>
2019-11-29 00:47:57 +0700

    Add files via upload

---
+++ 20190801347_Bukti.js
@@ -0,0 +1,12 @@
+console.log("Bukti");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+var x = ""
+for(var i = 0; i < 3; i++){
+    for(var j = 0; j < 8; j++){
+        x = x+ " * ";
+    }
+    console.log(x);
+    x = "";
+}
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~")
