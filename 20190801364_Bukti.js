f512a7e0c3e902878998c3874a810d9adcf7f764
hapiznuddin <55013162+hapiznuddin@users.noreply.github.com>
2019-11-28 21:26:05 +0700

    Add files via upload

---
+++ 20190801364_Bukti.js
@@ -0,0 +1,13 @@
+console.log("Bintang 3x8");
+
+      for(var i = 0; i < 3; i++) {
+        var x = '';
+        for(var j = 0; j < 8; j++) {
+          var x = x + ' * ';
+        }
+        console.log(x);
+      }
+    
+    
+
+  
