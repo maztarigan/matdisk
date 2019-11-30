2652d65709831a7cdb4b3dadf5bb32cc68dd224f
Lorenzo Padang <54989407+LorenzoPadang@users.noreply.github.com>
2019-11-30 19:10:09 +0700

    Add files via upload

---
+++ 20190801374_jamkerja.js
@@ -0,0 +1,50 @@
+console.log("Welcome");
+console.log("Menghitung lama jam kerja");
+var jamkerja = 
+    [[16.1,20.9],
+    [20.6,22],
+    [23,24],
+    [5,6.5],
+    [15,23],
+    [17,21],
+    [2,4],
+    [19.5,21.5],
+    [8,16],
+    [16,23],
+    [17,18.25],
+    [7,9],
+    [14,30],
+    [00,1.5],
+    [5.16,24]];
+var lamakerja = jamkerja[0][1] - jamkerja[0][0];
+var lamakerja1 =jamkerja[1][1] - jamkerja[1][0];
+var lamakerja2 =jamkerja[2][1] - jamkerja[2][0];
+var lamakerja3 =jamkerja[3][1] - jamkerja[3][0];
+var lamakerja4 =jamkerja[4][1] - jamkerja[4][0];
+var lamakerja5 =jamkerja[5][1] - jamkerja[5][0];
+var lamakerja6 =jamkerja[6][1] - jamkerja[6][0];
+var lamakerja7 =jamkerja[7][1] - jamkerja[7][0];
+var lamakerja8 =jamkerja[8][1] - jamkerja[8][0];
+var lamakerja9 =jamkerja[9][1] - jamkerja[9][0];
+var lamakerja10 =jamkerja[10][1] - jamkerja[10][0];
+var lamakerja11 =jamkerja[11][1] - jamkerja[11][0];
+var lamakerja12 =jamkerja[12][1] - jamkerja[12][0];
+var lamakerja13 =jamkerja[13][1] - jamkerja[13][0];
+var lamakerja14 =jamkerja[14][1] - jamkerja[14][0];
+console.log("Lama Jam Kerja")
+console.log("Dimas  : "+lamakerja +" jam");
+console.log("Raihan : "+lamakerja1+" jam");
+console.log("Ilham  : "+lamakerja2+" jam");
+console.log("Fikry  : "+lamakerja3+" jam");
+console.log("Jhordy : "+lamakerja4+" jam");
+console.log("Kresna : "+lamakerja5+" jam");
+console.log("Sutna  : "+lamakerja6+" jam");
+console.log("Tiara  : "+lamakerja7+" jam");
+console.log("Sindy  : "+lamakerja8+" jam");
+console.log("Nia    : "+lamakerja9+" jam");
+console.log("Lorenzo: "+lamakerja10+" jam");
+console.log("Anrian : "+lamakerja11+" jam");
+console.log("Alfian : "+lamakerja12+" jam");
+console.log("Andri  : "+lamakerja13+" jam");
+console.log("Androw : "+lamakerja14+" jam");
+console.log("Thank you");
