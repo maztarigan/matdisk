4fe671e453cc31212d7163a2b7b323c129802ef7
ALFIAN AJI WAHYUDI <45096863+alfianaji24@users.noreply.github.com>
2019-11-30 19:10:11 +0700

    Add files via upload

---
+++ 20190801422_LamaWaktu.js
@@ -0,0 +1,53 @@
+console.log("SELAMAT DATANG")
+console.log("ALFIAN AJI WAHYUDI")
+console.log("20190801422")
+console.log("Matematika Diskrit")
+console.log("-------------------------------------------------")
+var Absensi =
+     [[16.1,20.9],
+     [20.6,22],
+     [23,24],
+     [5,6.5],
+     [15,23],
+     [17,21],
+     [2,4],
+     [19.5,21.5],
+     [8,16],
+     [16,23],
+     [17,18.25],
+     [7,9],
+     [14,30],
+     [00,1.5],
+     [5.16,24]];
+var Data   = Absensi[0][1]   - Absensi[0][0];
+var Data1  = Absensi[1][1]   - Absensi[1][0];
+var Data2  = Absensi[2][1]   - Absensi[2][0];
+var Data3  = Absensi[3][1]   - Absensi[3][0];
+var Data4  = Absensi[4][1]   - Absensi[4][0];
+var Data5  = Absensi[5][1]   - Absensi[5][0];
+var Data6  = Absensi[6][1]   - Absensi[6][0];
+var Data7  = Absensi[7][1]   - Absensi[7][0];
+var Data8  = Absensi[8][1]   - Absensi[8][0];
+var Data9  = Absensi[9][1]   - Absensi[9][0];
+var Data10 = Absensi[10][1]  - Absensi[10][0];
+var Data11 = Absensi[11][1]  - Absensi[11][0];
+var Data12 = Absensi[12][1]  - Absensi[12][0];
+var Data13 = Absensi[13][1]  - Absensi[13][0];
+var Data14 = Absensi[14][1]  - Absensi[14][0];
+
+console.log("Lama waktu aktifitas")
+console.log("Dimas  : "+Data +" jam");
+console.log("Raihan : "+Data1+" jam");
+console.log("Ilham  : "+Data2+" jam");
+console.log("Fikry  : "+Data3+" jam");
+console.log("Jhordy : "+Data4+" jam");
+console.log("Kresna : "+Data5+" jam");
+console.log("Sutna  : "+Data6+" jam");
+console.log("Tiara  : "+Data7+" jam");
+console.log("Sindy  : "+Data8+" jam");
+console.log("Febri  : "+Data9+" jam");
+console.log("Lorenzo: "+Data10+" jam");
+console.log("Anrian : "+Data11+" jam");
+console.log("Alfian : "+Data12+" jam");
+console.log("Andri  : "+Data13+" jam");
+console.log("Androw : "+Data14+" jam");
