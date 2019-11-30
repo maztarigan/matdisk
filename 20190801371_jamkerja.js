b417f9544268aaef3807f7aec55b4d950bb7dde2
Raihan Ghifari <54989469+RaihanGhifari24@users.noreply.github.com>
2019-12-01 01:21:55 +0700

    Add files via upload

---
+++ 20190801371_jamkerja.js
@@ -0,0 +1,50 @@
+var absen =
+     [[16.1,20.9],
+     [20.6,22],
+     [23,24],
+     [5,6.5],
+     [15,23],
+     [17,21],
+     [2,4],
+     [19.5,21.5],
+     [8,16],
+     [16,23],
+     [17,18.25],
+     [7,9],
+     [14,30],
+     [00,1.5],
+     [5.16,24]];
+
+     var Data   = absen [0][1]   - absen [0][0];
+     var Data1  = absen [1][1]   - absen [1][0];
+     var Data2  = absen [2][1]   - absen [2][0];
+     var Data3  = absen [3][1]   - absen [3][0];
+     var Data4  = absen [4][1]   - absen [4][0];
+     var Data5  = absen [5][1]   - absen [5][0];
+     var Data6  = absen [6][1]   - absen [6][0];
+     var Data7  = absen [7][1]   - absen [7][0];
+     var Data8  = absen [8][1]   - absen [8][0];
+     var Data9  = absen [9][1]   - absen [9][0];
+     var Data10 = absen [10][1]  - absen [10][0];
+     var Data11 = absen [11][1]  - absen [11][0];
+     var Data12 = absen [12][1]  - absen [12][0];
+     var Data13 = absen [13][1]  - absen [13][0];
+     var Data14 = absen [14][1]  - absen [14][0];
+     
+console.log("Berapa lama jam kerja")
+
+console.log("DIMAS  : "+Data +" ");
+console.log("RAIHAN : "+Data1+" ");
+console.log("ILHAM  : "+Data2+" ");
+console.log("FIKRY  : "+Data3+" ");
+console.log("JHORDY : "+Data4+" ");
+console.log("KRESNA : "+Data5+" ");
+console.log("SUTNA  : "+Data6+" ");
+console.log("TIARA  : "+Data7+" ");
+console.log("SINDY  : "+Data8+" ");
+console.log("FEBRI  : "+Data9+" ");
+console.log("LORENZO: "+Data10+"");
+console.log("ANRIAN : "+Data11+"");
+console.log("ALFIAN : "+Data12+"");
+console.log("ANDRI  : "+Data13+"");
+console.log("ANDROW : "+Data14+"");
