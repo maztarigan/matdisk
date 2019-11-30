26077ff7755449194b16da1456be0a67fcdd6a18
Sindy Ayu Lestari <sindyayulestari69@gmail.com>
2019-11-30 20:00:06 +0700

    Add files via upload

---
+++ 20190801358_jamkerja.js
@@ -0,0 +1,53 @@
+console.log("SELAMAT NamaNG")
+console.log("Sindy Ayu lestari")
+console.log("20190801358")
+console.log("Matematika Diskrit")
+console.log("-------------------------------------------------")
+var Absenharian =
+     [[16.1,20.9],
+     [20.6,22],
+     [23,24],
+     [5,6.5],
+     [15,23],
+     [17,21],
+     [2,4],
+     [19.5,21.5],
+     [8,16],
+     [16,23],
+     [17,18.25],
+     [7,9],
+     [14,30],
+     [00,1.5],
+     [5.16,24]];
+var Nama   = Absenharian[0][1]   - Absenharian[0][0];
+var Nama1  = Absenharian[1][1]   - Absenharian[1][0];
+var Nama2  = Absenharian[2][1]   - Absenharian[2][0];
+var Nama3  = Absenharian[3][1]   - Absenharian[3][0];
+var Nama4  = Absenharian[4][1]   - Absenharian[4][0];
+var Nama5  = Absenharian[5][1]   - Absenharian[5][0];
+var Nama6  = Absenharian[6][1]   - Absenharian[6][0];
+var Nama7  = Absenharian[7][1]   - Absenharian[7][0];
+var Nama8  = Absenharian[8][1]   - Absenharian[8][0];
+var Nama9  = Absenharian[9][1]   - Absenharian[9][0];
+var Nama10 = Absenharian[10][1]  - Absenharian[10][0];
+var Nama11 = Absenharian[11][1]  - Absenharian[11][0];
+var Nama12 = Absenharian[12][1]  - Absenharian[12][0];
+var Nama13 = Absenharian[13][1]  - Absenharian[13][0];
+var Nama14 = Absenharian[14][1]  - Absenharian[14][0];
+
+console.log("Lama waktu aktifitas")
+console.log("Dimas  : "+Nama +" Jam");
+console.log("Raihan : "+Nama1+" Jam");
+console.log("Ilham  : "+Nama2+" Jam");
+console.log("Fikry  : "+Nama3+" Jam");
+console.log("Jhordy : "+Nama4+" Jam");
+console.log("Kresna : "+Nama5+" Jam");
+console.log("Sutna  : "+Nama6+" Jam");
+console.log("Tiara  : "+Nama7+" Jam");
+console.log("Sindy  : "+Nama8+" Jam");
+console.log("Febri  : "+Nama9+" Jam");
+console.log("Lorenzo: "+Nama10+" Jam");
+console.log("Anrian : "+Nama11+" Jam");
+console.log("Alfian : "+Nama12+" Jam");
+console.log("Andri  : "+Nama13+" Jam");
+console.log("Androw : "+Nama14+" Jam");
