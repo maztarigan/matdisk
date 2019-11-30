78857323af9b27f92f06fe429be3be312381b2eb
tiaraokta17 <54990376+tiaraoktaviarni06@users.noreply.github.com>
2019-11-30 19:29:51 +0700

    Add files via upload

---
+++ 20190801352_algoritma.js
@@ -0,0 +1,236 @@
+console.log("jadwal jam kerja");
+var jam_kerja = [
+     [16.1,20.9],
+     [20.6,22],
+     [23.00,24.00],
+     [5.00,6.5],
+     [15.00,23.00],
+     [17.00,21.00],
+     [2.00,4.00],
+     [19.5,21.5],
+     [08.00,16.00],
+     [16.00,23.00],
+     [17.00,18.25],
+     [07,09],
+     [14,30],
+     [0.0,1.30],
+     [5.16,24.00]];
+var lama_kerja = jam_kerja[0][1] - jam_kerja[0][0];
+var lama_kerja1 =jam_kerja[1][1] - jam_kerja[1][0];
+var lama_kerja2 =jam_kerja[2][1] - jam_kerja[2][0];
+var lama_kerja3 =jam_kerja[3][1] - jam_kerja[3][0];
+var lama_kerja4 =jam_kerja[4][1] - jam_kerja[4][0];
+var lama_kerja5 =jam_kerja[5][1] - jam_kerja[5][0];
+var lama_kerja6 =jam_kerja[6][1] - jam_kerja[6][0];
+var lama_kerja7 =jam_kerja[7][1] - jam_kerja[7][0];
+var lama_kerja8 =jam_kerja[8][1] - jam_kerja[8][0];
+var lama_kerja9 =jam_kerja[9][1] - jam_kerja[9][0];
+var lama_kerja10 =jam_kerja[10][1] - jam_kerja[10][0];
+var lama_kerja11 =jam_kerja[11][1] - jam_kerja[11][0];
+var lama_kerja12 =jam_kerja[12][1] - jam_kerja[12][0];
+var lama_kerja13 =jam_kerja[13][1] - jam_kerja[13][0];
+var lama_kerja14 =jam_kerja[14][1] - jam_kerja[14][0];
+console.log("Lama Jam Kerja")
+console.log("Dimas  : "+lama_kerja +" jam");
+console.log("Raihan : "+lama_kerja1+" jam");
+console.log("Ilham  : "+lama_kerja2+" jam");
+console.log("Fikry  : "+lama_kerja3+" jam");
+console.log("Jhordy : "+lama_kerja4+" jam");
+console.log("Kresna : "+lama_kerja5+" jam");
+console.log("Sutna  : "+lama_kerja6+" jam");
+console.log("Tiara  : "+lama_kerja7+" jam");
+console.log("Sindy  : "+lama_kerja8+" jam");
+console.log("Nia    : "+lama_kerja9+" jam");
+console.log("Loren  : "+lama_kerja10+" jam");
+console.log("Anrian : "+lama_kerja11+" jam");
+console.log("Alfian : "+lama_kerja12+" jam");
+console.log("Andri  : "+lama_kerja13+" jam");
+console.log("Androw : "+lama_kerja14+" jam");console.log("jadwal jam kerja");
+var jam_kerja = [
+     [16.1,20.9],
+     [20.6,22],
+     [23.00,24.00],
+     [5.00,6.5],
+     [15.00,23.00],
+     [17.00,21.00],
+     [2.00,4.00],
+     [19.5,21.5],
+     [08.00,16.00],
+     [16.00,23.00],
+     [17.00,18.25],
+     [07,09],
+     [14,30],
+     [0.0,1.30],
+     [5.16,24.00]];
+var lama_kerja = jam_kerja[0][1] - jam_kerja[0][0];
+var lama_kerja1 =jam_kerja[1][1] - jam_kerja[1][0];
+var lama_kerja2 =jam_kerja[2][1] - jam_kerja[2][0];
+var lama_kerja3 =jam_kerja[3][1] - jam_kerja[3][0];
+var lama_kerja4 =jam_kerja[4][1] - jam_kerja[4][0];
+var lama_kerja5 =jam_kerja[5][1] - jam_kerja[5][0];
+var lama_kerja6 =jam_kerja[6][1] - jam_kerja[6][0];
+var lama_kerja7 =jam_kerja[7][1] - jam_kerja[7][0];
+var lama_kerja8 =jam_kerja[8][1] - jam_kerja[8][0];
+var lama_kerja9 =jam_kerja[9][1] - jam_kerja[9][0];
+var lama_kerja10 =jam_kerja[10][1] - jam_kerja[10][0];
+var lama_kerja11 =jam_kerja[11][1] - jam_kerja[11][0];
+var lama_kerja12 =jam_kerja[12][1] - jam_kerja[12][0];
+var lama_kerja13 =jam_kerja[13][1] - jam_kerja[13][0];
+var lama_kerja14 =jam_kerja[14][1] - jam_kerja[14][0];
+console.log("Lama Jam Kerja")
+console.log("Dimas  : "+lama_kerja +" jam");
+console.log("Raihan : "+lama_kerja1+" jam");
+console.log("Ilham  : "+lama_kerja2+" jam");
+console.log("Fikry  : "+lama_kerja3+" jam");
+console.log("Jhordy : "+lama_kerja4+" jam");
+console.log("Kresna : "+lama_kerja5+" jam");
+console.log("Sutna  : "+lama_kerja6+" jam");
+console.log("Tiara  : "+lama_kerja7+" jam");
+console.log("Sindy  : "+lama_kerja8+" jam");
+console.log("Nia    : "+lama_kerja9+" jam");
+console.log("Loren  : "+lama_kerja10+" jam");
+console.log("Anrian : "+lama_kerja11+" jam");
+console.log("Alfian : "+lama_kerja12+" jam");
+console.log("Andri  : "+lama_kerja13+" jam");
+console.log("Androw : "+lama_kerja14+" jam");console.log("jadwal jam kerja");
+var jam_kerja = [
+     [16.1,20.9],
+     [20.6,22],
+     [23.00,24.00],
+     [5.00,6.5],
+     [15.00,23.00],
+     [17.00,21.00],
+     [2.00,4.00],
+     [19.5,21.5],
+     [08.00,16.00],
+     [16.00,23.00],
+     [17.00,18.25],
+     [07,09],
+     [14,30],
+     [0.0,1.30],
+     [5.16,24.00]];
+var lama_kerja = jam_kerja[0][1] - jam_kerja[0][0];
+var lama_kerja1 =jam_kerja[1][1] - jam_kerja[1][0];
+var lama_kerja2 =jam_kerja[2][1] - jam_kerja[2][0];
+var lama_kerja3 =jam_kerja[3][1] - jam_kerja[3][0];
+var lama_kerja4 =jam_kerja[4][1] - jam_kerja[4][0];
+var lama_kerja5 =jam_kerja[5][1] - jam_kerja[5][0];
+var lama_kerja6 =jam_kerja[6][1] - jam_kerja[6][0];
+var lama_kerja7 =jam_kerja[7][1] - jam_kerja[7][0];
+var lama_kerja8 =jam_kerja[8][1] - jam_kerja[8][0];
+var lama_kerja9 =jam_kerja[9][1] - jam_kerja[9][0];
+var lama_kerja10 =jam_kerja[10][1] - jam_kerja[10][0];
+var lama_kerja11 =jam_kerja[11][1] - jam_kerja[11][0];
+var lama_kerja12 =jam_kerja[12][1] - jam_kerja[12][0];
+var lama_kerja13 =jam_kerja[13][1] - jam_kerja[13][0];
+var lama_kerja14 =jam_kerja[14][1] - jam_kerja[14][0];
+console.log("Lama Jam Kerja")
+console.log("Dimas  : "+lama_kerja +" jam");
+console.log("Raihan : "+lama_kerja1+" jam");
+console.log("Ilham  : "+lama_kerja2+" jam");
+console.log("Fikry  : "+lama_kerja3+" jam");
+console.log("Jhordy : "+lama_kerja4+" jam");
+console.log("Kresna : "+lama_kerja5+" jam");
+console.log("Sutna  : "+lama_kerja6+" jam");
+console.log("Tiara  : "+lama_kerja7+" jam");
+console.log("Sindy  : "+lama_kerja8+" jam");
+console.log("Nia    : "+lama_kerja9+" jam");
+console.log("Loren  : "+lama_kerja10+" jam");
+console.log("Anrian : "+lama_kerja11+" jam");
+console.log("Alfian : "+lama_kerja12+" jam");
+console.log("Andri  : "+lama_kerja13+" jam");
+console.log("Androw : "+lama_kerja14+" jam");console.log("jadwal jam kerja");
+var jam_kerja = [
+     [16.1,20.9],
+     [20.6,22],
+     [23.00,24.00],
+     [5.00,6.5],
+     [15.00,23.00],
+     [17.00,21.00],
+     [2.00,4.00],
+     [19.5,21.5],
+     [08.00,16.00],
+     [16.00,23.00],
+     [17.00,18.25],
+     [07,09],
+     [14,30],
+     [0.0,1.30],
+     [5.16,24.00]];
+var lama_kerja = jam_kerja[0][1] - jam_kerja[0][0];
+var lama_kerja1 =jam_kerja[1][1] - jam_kerja[1][0];
+var lama_kerja2 =jam_kerja[2][1] - jam_kerja[2][0];
+var lama_kerja3 =jam_kerja[3][1] - jam_kerja[3][0];
+var lama_kerja4 =jam_kerja[4][1] - jam_kerja[4][0];
+var lama_kerja5 =jam_kerja[5][1] - jam_kerja[5][0];
+var lama_kerja6 =jam_kerja[6][1] - jam_kerja[6][0];
+var lama_kerja7 =jam_kerja[7][1] - jam_kerja[7][0];
+var lama_kerja8 =jam_kerja[8][1] - jam_kerja[8][0];
+var lama_kerja9 =jam_kerja[9][1] - jam_kerja[9][0];
+var lama_kerja10 =jam_kerja[10][1] - jam_kerja[10][0];
+var lama_kerja11 =jam_kerja[11][1] - jam_kerja[11][0];
+var lama_kerja12 =jam_kerja[12][1] - jam_kerja[12][0];
+var lama_kerja13 =jam_kerja[13][1] - jam_kerja[13][0];
+var lama_kerja14 =jam_kerja[14][1] - jam_kerja[14][0];
+console.log("Lama Jam Kerja")
+console.log("Dimas  : "+lama_kerja +" jam");
+console.log("Raihan : "+lama_kerja1+" jam");
+console.log("Ilham  : "+lama_kerja2+" jam");
+console.log("Fikry  : "+lama_kerja3+" jam");
+console.log("Jhordy : "+lama_kerja4+" jam");
+console.log("Kresna : "+lama_kerja5+" jam");
+console.log("Sutna  : "+lama_kerja6+" jam");
+console.log("Tiara  : "+lama_kerja7+" jam");
+console.log("Sindy  : "+lama_kerja8+" jam");
+console.log("Nia    : "+lama_kerja9+" jam");
+console.log("Loren  : "+lama_kerja10+" jam");
+console.log("Anrian : "+lama_kerja11+" jam");
+console.log("Alfian : "+lama_kerja12+" jam");
+console.log("Andri  : "+lama_kerja13+" jam");
+console.log("Androw : "+lama_kerja14+" jam");console.log("jadwal jam kerja");
+var jam_kerja = [
+     [16.1,20.9],
+     [20.6,22],
+     [23.00,24.00],
+     [5.00,6.5],
+     [15.00,23.00],
+     [17.00,21.00],
+     [2.00,4.00],
+     [19.5,21.5],
+     [08.00,16.00],
+     [16.00,23.00],
+     [17.00,18.25],
+     [07,09],
+     [14,30],
+     [0.0,1.30],
+     [5.16,24.00]];
+var lama_kerja = jam_kerja[0][1] - jam_kerja[0][0];
+var lama_kerja1 =jam_kerja[1][1] - jam_kerja[1][0];
+var lama_kerja2 =jam_kerja[2][1] - jam_kerja[2][0];
+var lama_kerja3 =jam_kerja[3][1] - jam_kerja[3][0];
+var lama_kerja4 =jam_kerja[4][1] - jam_kerja[4][0];
+var lama_kerja5 =jam_kerja[5][1] - jam_kerja[5][0];
+var lama_kerja6 =jam_kerja[6][1] - jam_kerja[6][0];
+var lama_kerja7 =jam_kerja[7][1] - jam_kerja[7][0];
+var lama_kerja8 =jam_kerja[8][1] - jam_kerja[8][0];
+var lama_kerja9 =jam_kerja[9][1] - jam_kerja[9][0];
+var lama_kerja10 =jam_kerja[10][1] - jam_kerja[10][0];
+var lama_kerja11 =jam_kerja[11][1] - jam_kerja[11][0];
+var lama_kerja12 =jam_kerja[12][1] - jam_kerja[12][0];
+var lama_kerja13 =jam_kerja[13][1] - jam_kerja[13][0];
+var lama_kerja14 =jam_kerja[14][1] - jam_kerja[14][0];
+console.log("Lama Jam Kerja")
+console.log("Dimas  : "+lama_kerja +" jam");
+console.log("Raihan : "+lama_kerja1+" jam");
+console.log("Ilham  : "+lama_kerja2+" jam");
+console.log("Fikry  : "+lama_kerja3+" jam");
+console.log("Jhordy : "+lama_kerja4+" jam");
+console.log("Kresna : "+lama_kerja5+" jam");
+console.log("Sutna  : "+lama_kerja6+" jam");
+console.log("Tiara  : "+lama_kerja7+" jam");
+console.log("Sindy  : "+lama_kerja8+" jam");
+console.log("Nia    : "+lama_kerja9+" jam");
+console.log("Loren  : "+lama_kerja10+" jam");
+console.log("Anrian : "+lama_kerja11+" jam");
+console.log("Alfian : "+lama_kerja12+" jam");
+console.log("Andri  : "+lama_kerja13+" jam");
+console.log("Androw : "+lama_kerja14+" jam");
