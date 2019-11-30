741bdeb93d3998996402b3df2dd912cfa09ee4d7
Androw Ragilson Soleh Aji Davisga <54989439+AndrowRagilson@users.noreply.github.com>
2019-11-30 20:03:38 +0700

    Add files via upload

---
+++ 20190801354_JamKerja.js
@@ -0,0 +1,54 @@
+console.log(" JAM KERJA ")
+console.log("ANDROW RAGILSON ")
+console.log("20190801354")
+
+var Absensi =
+     [[16.1,20.9],
+     [20.6,22],
+     [23,24],
+     [5,6.5],
+     [15,23],
+     [17,21],
+     [2,4],
+     [19.5,21.5],
+     [8,16],
+     [16,23],
+     [17,18.25],
+     [7,9],
+     [14,30],
+     [00,1.5],
+     [5.16,24]];
+
+var Data   = Absensi[0][1]   - Absensi[0][0];
+var Data1  = Absensi[1][1]   - Absensi[1][0];
+var Data2  = Absensi[2][1]   - Absensi[2][0];
+var Data3  = Absensi[3][1]   - Absensi[3][0];
+var Data4  = Absensi[4][1]   - Absensi[4][0];
+var Data5  = Absensi[5][1]   - Absensi[5][0];
+var Data6  = Absensi[6][1]   - Absensi[6][0];
+var Data7  = Absensi[7][1]   - Absensi[7][0];
+var Data8  = Absensi[8][1]   - Absensi[8][0];
+var Data9  = Absensi[9][1]   - Absensi[9][0];
+var Data10 = Absensi[10][1]  - Absensi[10][0];
+var Data11 = Absensi[11][1]  - Absensi[11][0];
+var Data12 = Absensi[12][1]  - Absensi[12][0];
+var Data13 = Absensi[13][1]  - Absensi[13][0];
+var Data14 = Absensi[14][1]  - Absensi[14][0];
+
+console.log("LAMA WAKTU KERJA");
+console.log("================================")
+console.log("DIMAS  : "+Data +" ");
+console.log("RAIHAN : "+Data1+" ");
+console.log("ILHAM  : "+Data2+" ");
+console.log("FIKRY  : "+Data3+" ");
+console.log("JHORDY : "+Data4+" ");
+console.log("KRESNA : "+Data5+" ");
+console.log("SUTNA  : "+Data6+" ");
+console.log("TIARA  : "+Data7+" ");
+console.log("SINDY  : "+Data8+" ");
+console.log("FEBRI  : "+Data9+" ");
+console.log("LORENZO: "+Data10+"");
+console.log("ANRIAN : "+Data11+"");
+console.log("ALFIAN : "+Data12+"");
+console.log("ANDRI  : "+Data13+"");
+console.log("ANDROW : "+Data14+"");
