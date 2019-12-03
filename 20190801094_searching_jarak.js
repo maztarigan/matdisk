f295584fa63bb57eea7fc1eb3612a3f46a5c3d3c
Andre Ardiansyah <54829724+andre-ard@users.noreply.github.com>
2019-12-03 12:59:32 +0700

    Add files via upload

---
+++ 20190801094_searching_jarak.js
@@ -0,0 +1,43 @@
+var input = require('readline-sync')
+
+var input_jarak = parseFloat(input.question("Masukan Jarak yang ingin dicari : "))
+
+var mhs = [
+    ["Fani",18,29],
+    ["Firlan",7,15],
+    ["Tomy",16,35],
+    ["Magfur",6,10],
+    ["Jeje",10,25],
+    ["Kevin",2,5],
+    ["Andri",8,22],
+    ["Syayid",18,32],
+    ["Latif",18,35],
+    ["Thoriq",6.2,17],
+    ["Andre",30,90],
+    ["Unggul",16,45],
+    ["Micko",1,15],
+    ["Mansur",8.4,21],
+    ["Levy",15.8,21.8],
+    ["Fadhly",12,25],
+    ["Rob",1,3],
+    ["Indra",14,45],
+    ["Dallas",0.5,2],
+    ["Diar",3.9,11],
+    ["Chandra",17,45],
+    ["Dana",2.5,15],
+    ["Vika",5.1,20],
+    ["Dimas",15,33],
+    ["Danang",1,5],
+    ["Sachrur",16,31],
+    ["Zahra",3,7]
+]
+
+for(var i=0; i<mhs.length; i++) {
+    if(mhs[i][1] == input_jarak) {
+        console.log("Nama  : "+mhs[i][0])
+        console.log("Jarak : "+mhs[i][1]+" km")
+        console.log("Waktu : "+mhs[i][2]+" menit")
+        break
+    }
+}
+
