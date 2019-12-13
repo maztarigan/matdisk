632ee73ad6276a0cd59c9319ae459ccd68158157
ALFIAN AJI WAHYUDI <45096863+alfianaji24@users.noreply.github.com>
2019-12-13 22:44:41 +0700

    Add files via upload

--- 20190801422_Birthday.js
+++ 20190801422_Birthday.js
@@ -13,25 +13,28 @@ console.log("");
 console.log("==================================")
 
 var Data =
-        [{"Nama":"Alfian","Tanggal_lahir":new Date('1997-12-19')},
-         {"Nama":"Andri","Tanggal_lahir":new Date('2000-02-29')},
-         {"Nama":"Androw","Tanggal_lahir":new Date('1997-10-28')},
-         {"Nama":"Anrian","Tanggal_lahir":new Date('1999-10-29')},
-         {"Nama":"Dimas","Tanggal_lahir":new Date('2000-06-21')},
-         {"Nama":"Febri","Tanggal_lahir":new Date('2001-12-14')},
-         {"Nama":"Fikry","Tanggal_lahir":new Date('1997-05-17')},
-         {"Nama":"Ilham","Tanggal_lahir":new Date('2001-03-01')},   
-         {"Nama":"Jhordy","Tanggal_lahir":new Date('2001-11-15')},    
-         {"Nama":"Kresna","Tanggal_lahir":new Date('2000-11-29')},
-         {"Nama":"Lorenzo","Tanggal_lahir":new Date('1996-06-26')},
-         {"Nama":"Raihan","Tanggal_lahir":new Date('2000-05-01')},   
-         {"Nama":"Sindy","Tanggal_lahir":new Date('2001-06-30')},
-         {"Nama":"Sutna","Tanggal_lahir":new Date('1998-12-24')},
-         {"Nama":"Tiara","Tanggal_lahir":new Date('2001-03-06')}];
+        [["Nama":"Alfian", parseInt(now.getYear() - new Date('1997-12-19').getYear())]
+         ["Nama":"Andri", parseInt(now.getYear() - new Date('2000-02-29').getYear())],
+         ["Nama":"Androw", parseInt(now.getYear() - new Date('1997-10-28').getYear())],
+         ["Nama":"Anrian", parseInt(now.getYear() - new Date('1999-10-29').getYear())],
+         ["Nama":"Dimas", parseInt(now.getYear() - new Date('2000-06-21').getYear())],
+         ["Nama":"Febri", parseInt(now.getYear() - new Date('2001-02-14').getYear())],
+         ["Nama":"Fikry", parseInt(now.getYear() - new Date('1997-05-17').getYear())],
+         ["Nama":"Ilham", parseInt(now.getYear() - new Date('2000-01-03').getYear())],   
+         ["Nama":"Jhordy", parseInt(now.getYear() - new Date('2001-11-15').getYear())],    
+         ["Nama":"Kresna", parseInt(now.getYear() - new Date('2000-11-29').getYear())],
+         ["Nama":"Lorenzo", parseInt(now.getYear() - new Date('1996-06-26').getYear())],
+         ["Nama":"Raihan", parseInt(now.getYear() - new Date('2000-05-01').getYear())],
+         ["Nama":"Sindy", parseInt(now.getYear() - new Date('2001-06-30').getYear())],
+         ["Nama":"Sutna", parseInt(now.getYear() - new Date('1998-12-24').getYear())],
+         ["Nama":"Tiara", parseInt(now.getYear() - new Date('2001-03-06').getYear())]];
 
-   for(i=0; i<Data.length; i++) {      
-        lahiran = Data[i].Tanggal_lahir;             
-        var diffDays = parseInt((now.getYear() - lahiran.getYear()));
-        console.log(Data[i].Nama + " saat ini berusia :" + diffDays + " Tahun");                                       
-     }
+        Data.sort((a,b) => {
+            if (a[1] > b[1]) return 1;
+            if (b[1] > a[1]) return -1;
+            return 0;
+        })
+for (i - 0; i < Data.length; i++){
+    console.log(Data[1][0] + "saat ini berusia :" + Data[i][1] + "Tahun");
+}
 console.log("==================================")
