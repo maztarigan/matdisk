55c33869de7140122b7c5d504b344e9aa9db4201
ALFIAN AJI WAHYUDI <45096863+alfianaji24@users.noreply.github.com>
2019-12-15 16:14:01 +0700

    Update 20190801422_Sortir.js

--- 20190801422_Sortir.js
+++ 20190801422_Sortir.js
@@ -6,35 +6,36 @@ console.log("-------------------------------------------------")
 var Data= require('dateformat');
 var today = Data("isoDate");
 var now = new Date();
-console.log("Tanggal saat ini : "+ today);
-console.log("");
 console.log("Umur Mahasiswa ESA UNGGUL");
+console.log("");
+console.log("Tanggal saat ini : "+ today);
 console.log("");
 console.log("==================================")
 
 var Data =
-        [["Nama":"Alfian", parseInt(now.getYear() - new Date('1997-12-19').getYear())]
-         ["Nama":"Andri", parseInt(now.getYear() - new Date('2000-02-29').getYear())],
-         ["Nama":"Androw", parseInt(now.getYear() - new Date('1997-10-28').getYear())],
-         ["Nama":"Anrian", parseInt(now.getYear() - new Date('1999-10-29').getYear())],
-         ["Nama":"Dimas", parseInt(now.getYear() - new Date('2000-06-21').getYear())],
-         ["Nama":"Febri", parseInt(now.getYear() - new Date('2001-02-14').getYear())],
-         ["Nama":"Fikry", parseInt(now.getYear() - new Date('1997-05-17').getYear())],
-         ["Nama":"Ilham", parseInt(now.getYear() - new Date('2000-01-03').getYear())],   
-         ["Nama":"Jhordy", parseInt(now.getYear() - new Date('2001-11-15').getYear())],    
-         ["Nama":"Kresna", parseInt(now.getYear() - new Date('2000-11-29').getYear())],
-         ["Nama":"Lorenzo", parseInt(now.getYear() - new Date('1996-06-26').getYear())],
-         ["Nama":"Raihan", parseInt(now.getYear() - new Date('2000-05-01').getYear())],
-         ["Nama":"Sindy", parseInt(now.getYear() - new Date('2001-06-30').getYear())],
-         ["Nama":"Sutna", parseInt(now.getYear() - new Date('1998-12-24').getYear())],
-         ["Nama":"Tiara", parseInt(now.getYear() - new Date('2001-03-06').getYear())]];
+        [["Alfian", parseInt(now.getYear() - new Date('1997-12-19').getYear())],
+         ["Andri", parseInt(now.getYear() - new Date('2000-02-29').getYear())],
+         ["Androw", parseInt(now.getYear() - new Date('1997-10-28').getYear())],
+         ["Anrian", parseInt(now.getYear() - new Date('1999-10-29').getYear())],
+         ["Dimas", parseInt(now.getYear() - new Date('2000-06-21').getYear())],
+         ["Febri", parseInt(now.getYear() - new Date('2001-02-14').getYear())],
+         ["Fikry", parseInt(now.getYear() - new Date('1997-05-17').getYear())],
+         ["Ilham", parseInt(now.getYear() - new Date('2000-01-03').getYear())],   
+         ["Jhordy", parseInt(now.getYear() - new Date('2001-11-15').getYear())],    
+         ["Kresna", parseInt(now.getYear() - new Date('2000-11-29').getYear())],
+         ["Lorenzo", parseInt(now.getYear() - new Date('1996-06-26').getYear())],
+         ["Raihan", parseInt(now.getYear() - new Date('2000-05-01').getYear())],
+         ["Sindy", parseInt(now.getYear() - new Date('2001-06-30').getYear())],
+         ["Sutna", parseInt(now.getYear() - new Date('1998-12-24').getYear())],
+         ["Tiara", parseInt(now.getYear() - new Date('2001-03-06').getYear())]];
 
-        Data.sort((a,b) => {
-            if (a[1] > b[1]) return 1;
-            if (b[1] > a[1]) return -1;
+        Data.sort((k,b) => {
+            if (k[1] > b[1]) return -1;
+            if (b[1] > k[1]) return 1;
             return 0;
         })
-for (i - 0; i < Data.length; i++){
-    console.log(Data[1][0] + "saat ini berusia :" + Data[i][1] + "Tahun");
+
+    for (i=0; i < Data.length; i++) {
+    console.log("" + Data[i][0] + " saat ini berusia : " + Data[i][1] + "Tahun");
 }
 console.log("==================================")
