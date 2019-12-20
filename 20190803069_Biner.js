23a40318bf13d21d200a39da81c6e785ef08a624
noval liani <54826685+novalueu2019@users.noreply.github.com>
2019-12-20 21:48:34 +0700

    Add files via upload

---
+++ 20190803069_Biner.js
@@ -0,0 +1,10 @@
+console.log("Noval Liani");
+console.log("NIM : 20190803069");
+console.log("\n");
+
+var key = require('readline-sync');
+var desimal = parseInt(key.question("masukkan nilai desimal : "));
+
+console.log("hasil");
+console.log(desimal.toString(2));
+
